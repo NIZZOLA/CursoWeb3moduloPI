93833ba56cd6407bf07f113b799bd67f20660aca
Marcio R Nizzola <marcio@nizzola.com.br>
2020-09-14 20:30:53 -0300

    Criação de Projeto básico MVC

---
+++ ProjetoNoticiasMVCEtec/ProjetoNoticiasMVCEtec/Migrations/20200914232117_criacaodenoticias.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjetoNoticiasMVCEtec.Migrations
+{
+    public partial class criacaodenoticias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Noticias",
+                columns: table => new
+                {
+                    NoticiaId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(maxLength: 100, nullable: true),
+                    DataDaInclusao = table.Column<DateTime>(nullable: false),
+                    Descricao = table.Column<string>(nullable: true),
+                    NomeDaImagem = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Noticias", x => x.NoticiaId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Noticias");
+        }
+    }
+}
